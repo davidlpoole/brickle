6890920efa14514a3139558abae2ca7bab499f25
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-04-17 21:28:38 +1200

    add conditional vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/brickle/',
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
+      base: '/brickle/',
+    }
+  }
 })
