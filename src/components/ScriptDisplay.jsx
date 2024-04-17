7f1f71c3dfc3d22130849db1f58758c7e67f7c68
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-04-17 19:43:09 +1200

    change script display to a textbox

--- src/components/ScriptDisplay.jsx
+++ src/components/ScriptDisplay.jsx
@@ -3,7 +3,13 @@ export default function ScriptDisplay(props) {
   return (
     <>
       <div>Script:</div>
-      <div id="scriptDisplay">{script.toUpperCase()}</div>
+      <input
+        type="text"
+        id="scriptDisplay"
+        value={script.toUpperCase()}
+        disabled
+        className="w-full px-4 py-2 bg-gray-700 text-white rounded-md mt-4 mb-4"
+      />
     </>
   )
 }
